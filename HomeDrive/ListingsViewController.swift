a1939897b61be85ddf1e605b19b7609851243727
Matthew <Matthew.b.rodriguez@gmail.com>
2018-11-25 03:02:41 -0500

    Edit launch screen

--- HomeDrive/ListingsViewController.swift
+++ HomeDrive/ListingsViewController.swift
@@ -24,11 +24,15 @@ class ListingsViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "HouseCell", for: indexPath as IndexPath)
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
